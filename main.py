dfe1a2fcc592b9e10522aed2270a78c3381fc6c0
Guilherme Ferreira Costa <guifcosta.contato@gmail.com>
2022-05-24 23:36:17 -0300

    Finalizado a auxiliar e as operacoes de eliminacao gaussina

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import table
 import numpy as np
 from scipy import linalg
 
@@ -36,17 +37,28 @@ for i in range(n):
 Adicionar as variaveis de folga
 '''
 
+C_original = -np.append(C, np.zeros(n))
+
+op_de_c = np.zeros(n)
+mat_operacoes = np.identity(n)
+
+''' Checar se tenho todas entradas positivas no b '''
+linhas_negativas_b = []
+for i in range(n):
+    if (B[i, 0] < 0):
+        B[i, 0] = -B[i, 0]
+        A[i,:] = -A[i,:]
+        mat_operacoes[i,:] = -mat_operacoes[i,:]
+        
+        
+#matriz A setada aqui
 folgas = np.identity(n)
 A_new = np.append(A, folgas, axis=1)
-C_new = np.append(C, np.zeros(n))
-
-print("C\n",  C_new)
-print("A\n",  A_new)
-print("b\n",  B)
 
 
+# CRIAR UMA FUNCAO para optimizar
 ''' 
-
+*** HOJE ***
 Gerar Tableux Auxiliar Extendida
 
 Enquanto C não estiver (negativa/positiva)
@@ -56,9 +68,91 @@ Enquanto C não estiver (negativa/positiva)
     4. se V.O menor que 0, então vetor de operações de C é o certificado de inviabilidade
     5. Se V.O igual a 0, prosseguimos
 '''
+# Gerando o tableux
+B_tableux = np.vstack((np.array([[0.]]), B))
+
+C_auxiliar = np.append(np.zeros(m), np.ones(n))
+A_C_auxiliar = np.vstack((C_auxiliar, A_new))
+ACB_auxiliar = np.hstack((A_C_auxiliar, B_tableux)) 
+
+A_C = np.vstack((C_original, A_new))
+ACB_original = np.hstack((A_C, B_tableux)) 
+
+# os componentes dessa matriz foram construidos acima
+mat_op_completa = np.vstack((op_de_c, mat_operacoes))
+
+tableux_original = np.hstack((mat_op_completa, ACB_original))
+tableux_auxiliar = np.hstack((mat_op_completa, ACB_auxiliar))
+
+
+print(tableux_auxiliar)
+print()
+print(tableux_original)
+print()
+
+
+''' Agora temos que deixar na forma canônica a PL Auxiliar '''
+bases_colunas = [n+m+1, n+m+2]
+for i in range(n):
+    tableux_auxiliar[0,:] = -tableux_auxiliar[i+1,:] + tableux_auxiliar[0,:]
+
+print("Forma Canonica") 
+print(tableux_auxiliar)
+
+input("Digite algo para continuar")
+
+''' Enquanto tivermos algum valor menor que 0 '''
+while ((tableux_auxiliar[0,n:-1] < 0).any()):
+    print("Entrei")
+    # varremos o c até encontrar um valor < 0
+    for i in range(n+m):
+        if tableux_auxiliar[0,n+i] < 0:
+            nova_base = n+i # nova base
+            print(nova_base)
+            break
+                        
+    # nao há ilimitada pois sabemos que a auxiliar é sempre limitada
+    menor = np.inf
+    linha_menor = 0
+    for j in range(n):
+        print("Aij = ",  tableux_auxiliar[j+1,nova_base])
+        if (tableux_auxiliar[j+1,nova_base] > 0): # Aij > 0
+            b_a = tableux_auxiliar[j+1, -1] / tableux_auxiliar[j+1,nova_base]
+            print("bij_Aij = ", b_a)
+            input("Seguir...")
+            if (b_a < menor): #se for igual nao muda (caimos no principio da não ciclagem)
+                input("Entrei no ultimo if")
+                menor = b_a
+                linha_menor = j+1
+
+    print(linha_menor)
+    print("elemento a ser pivoteado: ", tableux_auxiliar[linha_menor, nova_base])
+    # divimos a linha pelo pivô 
+    tableux_auxiliar[linha_menor,:] = tableux_auxiliar[linha_menor,:] * 1.0/tableux_auxiliar[linha_menor, nova_base]
+    print(tableux_auxiliar)
+    
+    # escalamento
+    # teremos a coluna e a linha que teremos que pivoterar
+    # Fazer eliminacao de gaus
+    for i in range(n+1):
+        if (i == linha_menor):
+            continue
+        # vamos zerar todos da coluna
+        bij = tableux_auxiliar[i, nova_base]
+        print(bij)
+        input("Aqui")
+        if (bij != 0): # não faço nada
+            tableux_auxiliar[i,:] = (-bij * tableux_auxiliar[linha_menor,:]) + tableux_auxiliar[i,:]
+           
+                
+    print(tableux_auxiliar)
+    print("----")
+    input("Pressione algo para ir para proxima iteracao...")              
+
 
 
 ''' 
+*** AMANHÂ *** 
 Gerar Tableux Original
 
 1. Tableux já vem na forma canônica e em FPI
--- teste.py
+++ teste.py
@@ -1,5 +1,7 @@
+from matplotlib.pyplot import axis
 import numpy as np
 n = 3
+m = 3
 # log_c = np.zeros(n)
 # matriz_de_operacoes = np.identity(n)
 
@@ -9,4 +11,35 @@ n = 3
 # t = np.hstack(())
 # print(aux)
 
-print(-np.ones(5))
+C = [2., 4., 8., 0., 0., 0.]
+
+A = np.array([
+ [1., 0., 0., 1., 0., 0.],
+ [0., 1., 0., 0., 1., 0.],
+ [0., 0., 1., 0., 0., 1.]]
+)
+
+B = np.array([
+    [1.],
+    [1.],
+    [1.]
+])
+
+B_tableux = np.vstack((np.array([[0.]]), B))
+A_C = np.vstack((C, A))
+ACB = np.hstack((A_C, B_tableux))
+print(ACB)
+
+op_de_c = np.zeros(n)
+mat_operacoes = np.identity(n)
+mat_op_completa = np.vstack((op_de_c, mat_operacoes))
+print(mat_op_completa)
+
+tableux = np.hstack((mat_op_completa, ACB))
+print(tableux)
+
+
+print(tableux[2, -1])
+# print(np.vstack((C, A)))
+# print(np.hstack((A, B)))
+# print(np.vstack((np.array([[0]]), B)))
